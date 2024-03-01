2e4b48f845e0520ee190b887499e937755e9d435
Ishizawa <daikonoisii2001@gmail.com>
2024-03-02 04:37:07 +0900

    階層構造の仮決定

--- api/main.py
+++ api/main.py
@@ -3,9 +3,9 @@ from typing import Union
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from s3 import image_interacter
+import  api_routes
 
 app = FastAPI()
-app.include_router(image_interacter.router)
 
 origins = [
      os.getenv("ACSESS_ALLOW_URL"),  # Next.jsアプリケーションのオリジン
@@ -20,11 +20,5 @@ app.add_middleware(
     allow_headers=["*"],  # すべてのヘッダーを許可
 )
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+app.include_router(image_interacter.router)
+app.include_router(api_routes.router)
