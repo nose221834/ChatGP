f0fe71d6f6508cbc1a4ff9097a7897ec14768caa
Ishizawa <daikonoisii2001@gmail.com>
2024-03-02 00:03:47 +0900

    Next.jsでFastAPIが使用できるようにした

--- api/main.py
+++ api/main.py
@@ -1,7 +1,24 @@
+import os
 from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+
+origins = [
+     os.getenv("ACSESS_ALLOW_URL"),  # Next.jsアプリケーションのオリジン
+    # 必要に応じて他のオリジンも追加
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # 許可するオリジンのリスト
+    allow_credentials=True,
+    allow_methods=["*"],  # すべてのメソッドを許可
+    allow_headers=["*"],  # すべてのヘッダーを許可
+)
 
 @app.get("/")
 def read_root():
