81669a4f936e1d23eb07467aae566c45cc234cf0
aiwaki <aiwaki@kashiwa.nagaokaut.ac.jp>
2024-03-02 08:52:02 +0000

    API Access IDも環境変数にしてセキュリティを向上させた

--- api/utils/auth.py
+++ api/utils/auth.py
@@ -2,10 +2,12 @@ import os
 from fastapi import Security, HTTPException
 from fastapi.security import APIKeyHeader
 
-api_key_header = APIKeyHeader(name="X-API-Key")
+api_access_id = os.getenv("API_ACCESS_ID")
+api_key_header = APIKeyHeader(name=api_access_id)
 
 def validate_api_key(api_key_header: str = Security(api_key_header)) -> str:
-    if api_key_header == os.getenv("API_ACCESS_KEY"):
+    api_access_key = os.getenv("API_ACCESS_KEY")
+    if api_key_header == api_access_key:
         return api_key_header
     raise HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
