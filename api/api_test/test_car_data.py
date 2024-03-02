ae9c655ad218583c27d2b0aadf17096e189e3e3b
Ishizawa <daikonoisii2001@gmail.com>
2024-03-03 02:38:19 +0900

    スペルミスの修正

--- api/api_test/test_car_data.py
+++ api/api_test/test_car_data.py
@@ -5,8 +5,8 @@ from fastapi.responses import Response
 router = APIRouter()
 
 
-@router.get("/{prayer}")
-def test_make_car(prayer: str,text: str):
+@router.get("/{player}")
+def test_make_car(player: str,text: str):
 
     with open('api_test/test_img_binary.bin', 'rb') as file:
         car_img_binary = file.read()
--- api/chat_gpt/car_data.py
+++ api/chat_gpt/car_data.py
@@ -5,8 +5,8 @@ from chat_gpt.image_generation import image_generate_chatgpt
 
 router = APIRouter()
 
-@router.get("/{prayer}")
-def make_car(prayer: str,text: str):
+@router.get("/{player}")
+def make_car(player: str,text: str):
     car_img_binary = image_generate_chatgpt(text)
 
     return Response(content=car_img_binary, media_type="image/png")
