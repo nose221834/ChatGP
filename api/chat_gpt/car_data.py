3bd76114d2525c69bc7ecf35024384af134ad651
aiwaki <aiwaki@kashiwa.nagaokaut.ac.jp>
2024-03-05 06:37:56 +0000

    エンドポイントをAPI Keyで保護し, 不要なエンドポイントをコメントアウト

--- api/chat_gpt/car_data.py
+++ api/chat_gpt/car_data.py
@@ -1,12 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Security
 from fastapi.responses import Response
 from chat_gpt.image_generation import image_generate_chatgpt
-
+from utils.auth import validate_api_key
 
 router = APIRouter()
 
 @router.get("/{player}")
-def make_car(player: str,text: str):
+def make_car(player: str,text: str, api_key: str = Security(validate_api_key)):
     url_car_img = image_generate_chatgpt(text)
-
     return {"url_car_img": url_car_img}
