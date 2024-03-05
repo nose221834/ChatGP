90706718e6e8c3e053ebe3d30cb1e0df43b4aa9c
Ishizawa <daikonoisii2001@gmail.com>
2024-03-05 20:42:29 +0900

    絶対車のイラストが出てくるようにプロンプトを修正

--- api/chat_gpt/image_generation.py
+++ api/chat_gpt/image_generation.py
@@ -5,13 +5,21 @@ import base64
 
 client = OpenAI()
 
+def shaping_prompts_car_img(text:str):
+
+    prompt="Draw a single car with a design based on the specified theme.#Theme# "+ text + " #Condition 1# Background is white. #Condition 2# The outline of the car is highlighted in black. #Condition 3# The theme must be reflected in the car's design, colors and decorations. For example, if the theme is [Cats are God!] then the car should include features and details that are reminiscent of cats (e.g., cat ears and tail shape, cat hair pattern design, etc.). However, the theme is not limited to this example, and the car design should be modified according to the theme. #Condition 4# Please depict only one car clearly, with no other objects displayed in the background."
+    return prompt
+
+
 #dall-e-2は使い物にならないので本番はdall-e-3を使用
 async def image_generate_chatgpt(text:str):
     
+    text_prompt = shaping_prompts_car_img(text)
+
     #modelはdell-e2
     response =  client.images.generate(
                         model   = "dall-e-2",   # モデル  
-                        prompt  = text,         # 画像生成に用いる説明文章         
+                        prompt  = text_prompt,         # 画像生成に用いる説明文章         
                         n       = 1,            # 何枚の画像を生成するか  
                         size="512x512",
                         quality="standard",
