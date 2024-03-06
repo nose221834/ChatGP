ab9ce0223ef41a7cfa8bbe98a0a3fda37b08ec8e
aiwaki <aiwaki@kashiwa.nagaokaut.ac.jp>
2024-03-06 18:30:36 +0000

    使用に沿ったキーに定数値とテスト関数を変更

--- view/src/lib/const.ts
+++ view/src/lib/const.ts
@@ -1,8 +1,8 @@
 // Player Car Constants
-export const PLAYER_CAR_IMAGE = "car_img";
-export const PLAYER_CAR_NAME = "name";
-export const PLAYER_CAR_LUCK = "luk";
-export const PLAYER_CAR_INSTRUCTION = "text_car_status";
+export const PLAYER_CAR_IMAGE = "player_car_image";
+export const PLAYER_CAR_NAME = "player_car_name";
+export const PLAYER_CAR_LUCK = "player_car_luck";
+export const PLAYER_CAR_INSTRUCTION = "player_car_instruction";
 // Enemy Car Constants
 export const ENEMY_CAR_IMAGE = "enemy_car_image";
 export const ENEMY_CAR_NAME = "enemy_car_name";
