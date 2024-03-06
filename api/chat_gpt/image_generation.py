3df1769659c415b2aa954884037322a754cdb6b4
Ishizawa <daikonoisii2001@gmail.com>
2024-03-06 16:29:53 +0900

    validateの戻り値を正常/異常(1/0)で統一ユーザーの入力にトークン数の上限を設けるvalidate_token_countを実装rembgが重すぎてInstallできなかった為,rembgの処理を取り除いたtest用の関数を実装.本番環境で消す予定.

--- api/chat_gpt/image_generation.py
+++ api/chat_gpt/image_generation.py
@@ -14,6 +14,7 @@ def shaping_prompts_car_img(text:str):
     prompt="Draw a single car with a design based on the specified theme.#Theme# "+ text + " #Condition 1# Background is white. #Condition 2# The outline of the car is highlighted in black. #Condition 3# The theme must be reflected in the car's design, colors and decorations. For example, if the theme is [Cats are God!] then the car should include features and details that are reminiscent of cats (e.g., cat ears and tail shape, cat hair pattern design, etc.). However, the theme is not limited to this example, and the car design should be modified according to the theme. #Condition 4# Please depict only one car clearly, with no other objects displayed in the background."
     return prompt
 
+
 async def image_generate_chatgpt(text:str):
     
     text_prompt = shaping_prompts_car_img(text)
@@ -39,5 +40,27 @@ async def image_generate_chatgpt(text:str):
     binary_image = buffered.getvalue() # 画像(バイナリ)を取得
     
     return b64encode(binary_image) # 画像(バイナリ)をbase64に変換して返す
+"""
 
+async def image_generate_chatgpt_no_rembg(text:str):
+    
+    text_prompt = shaping_prompts_car_img(text)
+    #modelはdell-e2
+    response =  client.images.generate(
+                        model   = "dall-e-2",   # モデル  
+                        prompt  = text_prompt,         # 画像生成に用いる説明文章         
+                        n       = 1,            # 何枚の画像を生成するか  
+                        #size="1024x1024",
+                        size="256x256",
+                        quality="standard",
+                    )
+    
+    image_url = response.data[0].url
+
+    # URLから画像(バイナリ)を取得
+    car_img_binary = requests.get(image_url).content
+    
+    
+    return b64encode(car_img_binary) # 画像(バイナリ)をbase64に変換して返す
+"""
 
