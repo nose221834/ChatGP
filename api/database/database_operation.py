d22c26d9aa854e748138c24a8085a73c9bd2c38f
Ishizawa <daikonoisii2001@gmail.com>
2024-03-07 17:22:27 +0900

    車の選択をランダムに行うように変更

--- api/database/database_operation.py
+++ api/database/database_operation.py
@@ -34,6 +34,18 @@ def get_data(db:str,table:str,key:str,id:int) -> list:
     conn.close()
     return results_list
 
+def count_record(db:str,table:str,key:str) -> int:
+    conn = connect_database(db)
+    c = conn.cursor()
+
+    check_effectiveness([table,key])
+
+    query = f"SELECT COUNT({key}) FROM {table}"
+    c.execute(query)
+    count = c.fetchone()[0]
+
+    conn.close()
+    return count
 
 if __name__ == '__main__':
     #add_data('car.db',"INSERT INTO enemy_car_data (car_id, path_img, name, luk, text) VALUES (2, 'database/car_img/car2.png', 'Feline Fury', 4, 'With its sleek exterior, cozy interior, and advanced features such as a built-in laser pointer for entertainment, this car is perfectly designed for cat lovers. You can be assured that every drive will feel like a catwalk. Meowvelous!')")
