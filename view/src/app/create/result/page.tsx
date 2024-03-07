499c00e7385b73e9de77547095cee557fa35485c
aiwaki <aiwaki@kashiwa.nagaokaut.ac.jp>
2024-03-07 09:03:27 +0000

    作成画面のベースを作成

--- view/src/app/create/result/page.tsx
+++ view/src/app/create/result/page.tsx
@@ -1,15 +1,49 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { Card } from "@/components/ui/card";
+import {
+  PLAYER_CAR_IMAGE,
+  PLAYER_CAR_NAME,
+  PLAYER_CAR_LUCK,
+  PLAYER_CAR_INSTRUCTION,
+  PLAYER_CAR_FORTUNE,
+} from "@/lib/const";
 
 export default function Home() {
-  const image = localStorage.getItem("UserCar");
+  const image = localStorage.getItem(PLAYER_CAR_IMAGE);
   const router = useRouter();
   if (image) {
     return (
-      <div>
-        <img src={image}></img>
-      </div>
+      <main>
+        <div className="flex flex-wrap justigy-around items-center h-screen bg-basecolor">
+          <div className="h-4/5 w-1/2 p-4">
+            <div className="flex flex-col justify-around items-center h-full w-full bg-primarycolor rounded-xl border-4 border-accentcolor">
+              <Card className="text-3xl text-center w-11/12 border-4 border-basecolor p-8">
+                <p>俺の名前は{localStorage.getItem(PLAYER_CAR_NAME)}!!</p>
+                <p>
+                  {PLAYER_CAR_FORTUNE[localStorage.getItem(PLAYER_CAR_LUCK)]}
+                </p>
+                <p>{localStorage.getItem(PLAYER_CAR_INSTRUCTION)}</p>
+              </Card>
+            </div>
+          </div>
+          <div className="h-4/5 w-1/2 p-4">
+            <div className="flex flex-col justify-around items-center h-full w-full bg-primarycolor rounded-xl border-4 border-accentcolor">
+              <Image
+                src={image}
+                width={700}
+                height={700}
+                alt="player_car_image"
+              />
+            </div>
+          </div>
+        </div>
+      </main>
+      // <div>
+      //   <img src={image}></img>
+      // </div>
     );
   } else {
     router.push("/create");
--- view/src/lib/const.ts
+++ view/src/lib/const.ts
@@ -3,6 +3,16 @@ export const PLAYER_CAR_IMAGE = "player_car_image";
 export const PLAYER_CAR_NAME = "player_car_name";
 export const PLAYER_CAR_LUCK = "player_car_luck";
 export const PLAYER_CAR_INSTRUCTION = "player_car_instruction";
+export const PLAYER_CAR_FORTUNE = 
+    {   
+        0: "最悪の運勢だぜ！",
+        1: "不運な日だな...",
+        2: "まあまあの運勢だぜ！",
+        3: "普通の日だな...",
+        4: "結構いい日だぜ！",
+        5: "いい日だな！",
+        6: "最高の運勢だぜ！"
+    }
 // Enemy Car Constants
 export const ENEMY_CAR_IMAGE = "enemy_car_image";
 export const ENEMY_CAR_NAME = "enemy_car_name";
