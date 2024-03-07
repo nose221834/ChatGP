d438e8102c23198efffab5cdedbb66c64fbeb443
aiwaki <aiwaki@kashiwa.nagaokaut.ac.jp>
2024-03-07 14:00:59 +0000

    気分を表す文章を修正,吹き出しなしで結果画面を実装

--- view/src/app/create/result/page.tsx
+++ view/src/app/create/result/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+
 import Image from "next/image";
 import { Card } from "@/components/ui/card";
 import {
@@ -18,17 +19,18 @@ export default function Home() {
     return (
       <main>
         <div className="flex flex-wrap justigy-around items-center h-screen bg-basecolor">
-          <div className="h-4/5 w-1/2 p-4">
-            <div className="flex flex-col justify-around items-center h-full w-full bg-primarycolor rounded-xl border-4 border-accentcolor">
-              <Card className="text-3xl text-center w-11/12 border-4 border-basecolor p-8">
-                <p>俺の名前は{localStorage.getItem(PLAYER_CAR_NAME)}!!</p>
-                <p>
-                  {PLAYER_CAR_FORTUNE[localStorage.getItem(PLAYER_CAR_LUCK)]}
-                </p>
+          <div className="flex h-full w-1/2 p-4 flex-col justify-around items-center">
+            <div className="text-3xl tracking-wider text-center w-11/12 p-4 items-center bg-secondarycolor text-basecolor rounded-xl border-4 border-accentcolor">
+              <div className="p-4 text-left">
                 <p>{localStorage.getItem(PLAYER_CAR_INSTRUCTION)}</p>
-              </Card>
+              </div>
             </div>
+            <Card className="text-4xl tracking-wider text-center w-11/12 border-4 border-accentcolor p-8">
+              <p>俺は、{localStorage.getItem(PLAYER_CAR_NAME)}</p>
+              <p>今日は、{PLAYER_CAR_FORTUNE[6]}</p>
+            </Card>
           </div>
+
           <div className="h-4/5 w-1/2 p-4">
             <div className="flex flex-col justify-around items-center h-full w-full bg-primarycolor rounded-xl border-4 border-accentcolor">
               <Image
