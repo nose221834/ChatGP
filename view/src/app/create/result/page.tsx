3c3e17a6c131379de1c1ce5d028cc8323ad6f904
aiwaki <aiwaki@kashiwa.nagaokaut.ac.jp>
2024-03-08 22:24:14 +0900

    GPTの返り値の検証処理を追加

--- view/src/app/create/result/page.tsx
+++ view/src/app/create/result/page.tsx
@@ -13,31 +13,42 @@ import {
 } from "@/lib/const";
 
 export default function Home() {
-  const image = localStorage.getItem(PLAYER_CAR_IMAGE);
+  const carImage = localStorage.getItem(PLAYER_CAR_IMAGE);
+  const carName = localStorage.getItem(PLAYER_CAR_NAME);
+  let carLuck = localStorage.getItem(PLAYER_CAR_LUCK);
+  const carInstruction = localStorage.getItem(PLAYER_CAR_INSTRUCTION);
+  let carFortune;
+  if(carLuck){
+    // carLuck to int
+    carLuck = parseInt(carLuck);
+    if(1 <= carLuck && carLuck <= 6){
+      carFortune = PLAYER_CAR_FORTUNE[carLuck];
+    }
+  }
   const router = useRouter();
-  if (image) {
+  if (carImage && carName && carFortune) {
     return (
       <main>
         <div className="flex flex-wrap justigy-around items-center h-screen bg-basecolor">
           <div className="flex h-full w-1/2 p-4 flex-col justify-around items-center">
             <div className="text-3xl tracking-wider text-center w-11/12 p-4 items-center bg-secondarycolor text-basecolor rounded-xl border-4 border-accentcolor">
               <div className="p-4 text-left">
-                <p>{localStorage.getItem(PLAYER_CAR_INSTRUCTION)}</p>
+                <p>{carInstruction}</p>
               </div>
             </div>
             <Card className="text-4xl tracking-wider text-center w-11/12 border-4 border-accentcolor p-8">
-              <p>俺は、{localStorage.getItem(PLAYER_CAR_NAME)}</p>
-              <p>今日は、{PLAYER_CAR_FORTUNE[localStorage.getItem(PLAYER_CAR_LUCK)]}</p>
+              <p>俺は、{carName}</p>
+              <p>今日は、{carFortune}</p>
             </Card>
           </div>
 
           <div className="h-4/5 w-1/2 p-4">
             <div className="flex flex-col justify-around items-center h-full w-full bg-primarycolor rounded-xl border-4 border-accentcolor">
               <Image
-                src={image}
+                src={carImage}
                 width={700}
                 height={700}
-                alt="player_car_image"
+                alt={PLAYER_CAR_IMAGE}
               />
             </div>
           </div>
