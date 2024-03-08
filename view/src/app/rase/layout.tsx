0b660adbda5969327227ed4e38f3e32675c333a9
nose221834 <22.t.ichinose.nutfes@gmail.com>
2024-03-08 14:09:42 +0900

    race以下にlayout.tsxを作成

--- view/src/app/rase/layout.tsx
+++ view/src/app/rase/layout.tsx
@@ -6,21 +6,17 @@ export default function RaceLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
-      <body className="">
-        <div className="flex flex-col items-center bg-basecolor absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-4 border-accentcolor rounded-xl">
-          <Image
-            src="/announcer.webp"
-            alt="announcer"
-            width={196}
-            height={196}
-            priority
-            className=""
-          />
-        </div>
-        {children}
-      </body>
-    </html>
+    <div>
+      <div className="bg-basecolor absolute top-0 left-0 border-4 border-accentcolor rounded-xl">
+        <Image
+          src="/announcer.webp"
+          alt="announcer"
+          width={192}
+          height={192}
+          priority
+        />
+      </div>
+      {children}
+    </div>
   );
 }
