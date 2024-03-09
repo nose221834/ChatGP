8a2b09051216a38cb3214af854cc12eaa6a02eba
Ishizawa <daikonoisii2001@gmail.com>
2024-03-10 07:29:10 +0900

    ゲーム終了ボタンを配置

--- view/src/app/ending/page.tsx
+++ view/src/app/ending/page.tsx
@@ -1,12 +1,26 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
 
+    const router = useRouter();
+
+    const moveToStart = () => {
+        router.push("/create");
+      }
+      
     return (
       <main>
-       <div className="bg-[url('/race2.webp')] bg-cover h-screen w-full">
-        <button>エンディング</button>
-       </div>
+        <div className="bg-[url('/race2.webp')] bg-cover h-screen w-full p-4">
+          <div className="absolute bottom-0 right-0 m-4">
+            <Button className="border bg-transparent hover:bg-secondarycolor text-basecolor w-30 h-16 text-xl text-center tracking-widest"
+            onClick={moveToStart}>
+                Game End
+            </Button>
+          </div>
+
+        </div>
       </main>
     );
 }
