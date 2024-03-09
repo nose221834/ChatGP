8cd048395e7b7856a60f92ac238c546fee4d33c1
aiwaki <aiwaki@kashiwa.nagaokaut.ac.jp>
2024-03-09 23:03:20 +0900

    Race中のInteractiveな操作に関するAPIを叩くためのServer Actionを作成

---
+++ view/src/lib/race/action.ts
@@ -0,0 +1,32 @@
+"use server";
+
+import { RaceData, RaceEndData, RaceInfoRes } from '@/app/race/type'
+
+const apiId = process.env.NEXT_PUBLIC_API_ACCESS_ID;
+const apiKey = process.env.NEXT_PUBLIC_API_ACCESS_KEY;
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+// レース中のインタラクティブな入出力に関するアクションを定義する
+export const getRaceDataFromGpt = async (data: RaceData) =>{
+    if (!apiId || !apiKey || !apiUrl) return false;
+    const endPoint = `${apiUrl}/race/middle_part`;
+    console.log("Endpoint:", endPoint);
+    const responseJson: RaceInfoRes = await fetch(endPoint, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        [apiId]: apiKey,
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .catch((err) => {
+        console.error("Error:", err);
+        return false;
+      });
+    return responseJson;
+}
+
+export const getEndDataFromGpt = async (data: RaceEndData) =>{
+
+}
