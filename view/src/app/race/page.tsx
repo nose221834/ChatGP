742f4889ecd1d7762d42e03cc3923a2e00a0e077
nose221834 <22.t.ichinose.nutfes@gmail.com>
2024-03-10 04:15:44 +0900

    localStrageに初期のRACE_RESPONSE_DATAを保存する

--- view/src/app/race/page.tsx
+++ view/src/app/race/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Interactive } from "./Interactive";
 import { Progress } from "./Progress";
 import { useRouter } from "next/navigation";
@@ -20,20 +20,22 @@ import {
 import { PlayerCarRes } from "@/app/create/type";
 
 export default function Home() {
+  // Sample RaceData
+  const sampleRaceData: RaceData = {
+    first_car_name: "string",
+    second_car_name: "string",
+    third_car_name: "string",
+    fourth_car_name: "string",
+    player_car_name: "string",
+    first_car_instruction: "string",
+    second_car_instruction: "string",
+    third_car_instruction: "string",
+    fourth_car_instruction: "string",
+    event: "event",
+  };
+
   const testGetRaceInfoFromGpt = async (event: string) => {
     // Sample RaceData
-    const sampleRaceData: RaceData = {
-      "first_car_name": "string",
-      "second_car_name": "string",
-      "third_car_name": "string",
-      "fourth_car_name": "string",
-      "player_car_name": "string",
-      "first_car_instruction": "string",
-      "second_car_instruction": "string",
-      "third_car_instruction": "string",
-      "fourth_car_instruction": "string",
-      "event": event
-    };
     console.log("sampleRaceData", JSON.stringify(sampleRaceData));
     const responseJson = await getRaceDataFromGpt(sampleRaceData);
     console.log("responseJson:", responseJson);
@@ -48,8 +50,7 @@ export default function Home() {
 
   async function onSubmit(data: SubmitProps) {
     if (scene + 1 >= 3) {
-      console.log("data:", data.event);
-
+      console.log("scene + 1 >= 3");
       const previousResponce = localStorage.getItem(RACE_RESPONSE_DATA);
       if (previousResponce) {
         const previousJson = JSON.parse(previousResponce) as RaceData;
@@ -60,41 +61,41 @@ export default function Home() {
         // const responseJson = await getEndDataFromGpt(sendJson);
         // localStorage.setItem(RACE_RESPONSE_DATA, JSON.stringify(responseJson));
         router.push("/race/ending");
-      } else {
-        const previousResponce = localStorage.getItem(RACE_RESPONSE_DATA);
-        if (previousResponce) {
-          const previousJson = JSON.parse(previousResponce) as RaceData;
-          const sendJson = {
-            ...previousJson,
-            [RACE_EVENT]: data.event,
-          };
-          const responseJson = await getRaceDataFromGpt(sendJson);
-          if (responseJson) return Error;
-          localStorage.setItem(
-            RACE_RESPONSE_DATA,
-            JSON.stringify(responseJson)
-          );
-          setResponse(true);
-        }
+      }
+    } else {
+      console.log("router.push()");
+      const previousResponce = localStorage.getItem(RACE_RESPONSE_DATA);
+      if (previousResponce) {
+        console.log(previousResponce, "previousResponce");
+        const previousJson = JSON.parse(previousResponce) as RaceData;
+        const sendJson = {
+          ...previousJson,
+          [RACE_EVENT]: data.event,
+        };
+        const responseJson = await getRaceDataFromGpt(sendJson);
+        if (!responseJson) return <div>Error</div>;
+        localStorage.setItem(RACE_RESPONSE_DATA, JSON.stringify(responseJson));
+        setResponse(true);
       }
     }
-
-    function nextScene(): void {
-      setScene(scene + 1);
-      setResponse(false);
-    }
-
-    if (!response) {
-      return (
-        <main>
-          <Interactive order={1} scene={scene} submit={onSubmit} />
-        </main>
-      );
-    } else
-      return (
-        <main>
-          <Progress order={1} scene={scene} click={nextScene} />
-        </main>
-      );
   }
+
+  function nextScene(): void {
+    setScene(scene + 1);
+    setResponse(false);
+  }
+
+  console.log(response, "response");
+  if (!response) {
+    return (
+      <main>
+        <Interactive order={1} scene={scene} submit={onSubmit} />
+      </main>
+    );
+  } else
+    return (
+      <main>
+        <Progress order={1} scene={scene} click={nextScene} />
+      </main>
+    );
 }
