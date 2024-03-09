07ee6c00538a98c4134bb0059af1c77dccc98048
aiwaki <aiwaki@kashiwa.nagaokaut.ac.jp>
2024-03-09 23:55:48 +0900

    Enemyの車の情報をValidateする関数の追加

--- view/src/lib/validator/carDatavalidator.ts
+++ view/src/lib/validator/carDatavalidator.ts
@@ -1,9 +1,14 @@
-import { PlayerCarRes } from "@/app/create/type";
+import { PlayerCarRes, EnemyCarRes } from "@/app/create/type";
 import {
   PLAYER_CAR_IMAGE,
   PLAYER_CAR_NAME,
   PLAYER_CAR_LUCK,
   PLAYER_CAR_INSTRUCTION,
+
+  ENEMY_CAR_IMAGE,
+  ENEMY_CAR_NAME,
+  ENEMY_CAR_LUCK,
+  ENEMY_CAR_INSTRUCTION,
 } from "@/lib/const";
 
 const toBlob = async (base64: string) => {
@@ -38,8 +43,32 @@ export const validatePlayerCarRes = async(carDataJson: PlayerCarRes) => {
     const url = URL.createObjectURL(blob);
     // ImageをUrlに変更したJSONを返却
     const carDataJsonWithUrl = {
-      ...carDataJson,
-      [PLAYER_CAR_IMAGE]: url,
+      ...carDataJson, // スプレッド構文を用いて、元のオブジェクトを展開
+      [PLAYER_CAR_IMAGE]: url, // ImageのみURLで上書き
     };
     return carDataJsonWithUrl;
 };
+
+export const validateEnemyCarRes = async(carDataJson: EnemyCarRes) => {
+    const dataBase64 = carDataJson[ENEMY_CAR_IMAGE];
+    const enemyCarName = carDataJson[ENEMY_CAR_NAME];
+    const enemyCarLuck = carDataJson[ENEMY_CAR_LUCK];
+    const enemyCarInstruction = carDataJson[ENEMY_CAR_INSTRUCTION];
+    // 文字型かどうかのチェック
+    if (typeof dataBase64 !== "string") return false;
+    if (typeof enemyCarName !== "string") return false;
+    if (typeof enemyCarInstruction !== "string") return false;
+    // 数値型かどうかのチェック
+    if (typeof enemyCarLuck !== "number") return false;
+    // base64をblobに変換
+    const blob = await toBlob(dataBase64);
+    if (!blob) return false; // 変換に失敗した場合
+    // blobをurlに変換
+    const url = URL.createObjectURL(blob);
+    // ImageをUrlに変更したJSONを返却
+    const carDataJsonWithUrl = {
+      ...carDataJson, // スプレッド構文を用いて、元のオブジェクトを展開
+      [ENEMY_CAR_IMAGE]: url, // ImageのみURLで上書き
+    };
+    return carDataJsonWithUrl;
+}
