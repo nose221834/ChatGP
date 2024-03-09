167b021864164ca31b09b714640fa2e956865097
Ishizawa <daikonoisii2001@gmail.com>
2024-03-10 07:03:18 +0900

    背景を設定

---
+++ view/src/app/ending/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function Home() {
+
+    return (
+      <main>
+       <div className="bg-[url('/race2.webp')] bg-cover h-screen w-full">
+        <button>エンディング</button>
+       </div>
+      </main>
+    );
+}
