e94fdcaaa3b2402b3830fc1b404a9b684d674e12
nose221834 <22.t.ichinose.nutfes@gmail.com>
2024-03-10 13:35:31 +0900

    ボタンのレイアウト調整

--- view/src/app/page.tsx
+++ view/src/app/page.tsx
@@ -2,18 +2,25 @@
 
 import { useRouter } from "next/navigation";
 import { getEnemyCar } from "@/app/getEnemyCar";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  const router = useRouter()
+  const router = useRouter();
   const moveToCreate = () => {
     localStorage.clear();
     getEnemyCar();
     router.push("/create");
-  }
+  };
   return (
-    <button
-      onClick={moveToCreate}>
-      button
-    </button>
-  )
+    <main>
+      <div className=" flex flex-col items-center justify-around  h-screen w-screen bg-basecolor">
+        <Button
+          onClick={moveToCreate}
+          className=" w-80 h-48 text-4xl tracking-widest items-center"
+        >
+          スタート！！
+        </Button>
+      </div>
+    </main>
+  );
 }
