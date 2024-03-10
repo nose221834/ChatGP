758a732567d0d273389ccdc3108c0d27fc728220
poporu <aobaiwaki@gmail.com>
2024-03-10 03:54:22 +0000

    Playerの順位を返す関数を作成

---
+++ view/src/lib/race/getPlayerRank.ts
@@ -0,0 +1,48 @@
+import { RaceInfoRes } from "@/app/race/type";
+
+import {
+    RACE_RESPONSE_DATA,
+    FIRST_PLACE,
+    SECOND_PLACE,
+    THIRD_PLACE,
+    FOURTH_PLACE,
+    PLAYER_CAR,
+    PLAYER_CAR_NAME,
+} from "@/lib/const";
+
+export const getPlayerRank = () => {
+    // PLAYER_NAMEの取得
+    const playerCar = localStorage.getItem(PLAYER_CAR);
+    if (playerCar === null) {
+        throw new Error("Player Car Data is not found.");
+    }
+    const playerCarName: string = JSON.parse(playerCar)[PLAYER_CAR_NAME];
+    // PLAYER_NAMEに対応する順位を取得(関数)
+    const playerRank = getRank(playerCarName);
+    return playerRank;
+};
+
+const getRank = (carName: string) => {
+    const responseJson = getResponseJson();
+    if(responseJson[FIRST_PLACE] === carName) {
+        return 1;
+    }
+    if(responseJson[SECOND_PLACE] === carName) {
+        return 2;
+    }
+    if(responseJson[THIRD_PLACE] === carName) {
+        return 3;
+    }
+    if(responseJson[FOURTH_PLACE] === carName) {
+        return 4;
+    }
+};
+
+const getResponseJson = () => {
+    const responseJson = localStorage.getItem(RACE_RESPONSE_DATA);
+    if (responseJson === null) {
+        // 何もない場合は、エラーをthrowする
+        throw new Error("Response Data is not found.");
+    }
+    return JSON.parse(responseJson) as RaceInfoRes;
+};
