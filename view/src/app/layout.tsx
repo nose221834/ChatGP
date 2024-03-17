f732a38288afaa72fec23ad9552075baf9048985
nose221834 <22.t.ichinose.nutfes@gmail.com>
2024-03-17 14:33:10 +0900

    fontを有効にする

--- view/src/app/layout.tsx
+++ view/src/app/layout.tsx
@@ -18,7 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <body className={cn("min-h-screen bg-background font-sans antialiased")}>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
         {children}
       </body>
     </html>
