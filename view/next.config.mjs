f95de0ebbc5d3580ba267bdab4db5e51ff76fd58
nose221834 <22.t.ichinose.nutfes@gmail.com>
2024-03-21 00:35:30 +0900

    本番環境でconsole.*が削除されるように変更

--- view/next.config.mjs
+++ view/next.config.mjs
@@ -1,4 +1,42 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/**
+ * @type {import('next').NextConfig}
+ */
 
+import { PHASE_DEVELOPMENT_SERVER } from "next/constants.js";
+import removeImports from "next-remove-imports";
+import TerserPlugin from "terser-webpack-plugin";
+
+const removeImportsFun = removeImports({
+  test: /node_modules([\s\S]*?)\.(tsx|ts|js|mjs|jsx)$/,
+  matchImports: "\\.(less|css|scss|sass|styl)$",
+});
+
+const nextConfig = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      /* 開発フェーズでのみ利用するオプションを設定 */
+    };
+  }
+
+  return {
+    /* 開発フェーズを除く全てのフェーズで有効なオプションを設定 */
+    webpack: (config, options) => {
+      // ----- ここから本番環境で、console.log を出力しない系の設定 -----
+      config.optimization.minimize = true;
+      config.optimization.minimizer = [
+        new TerserPlugin({
+          // Build時に console.log を削除する
+          terserOptions: {
+            compress: { drop_console: true },
+          },
+          // LICENSE 情報を残してその他のコメントを削除する
+          extractComments: "all",
+        }),
+      ];
+      // ----- ここまで本番環境で、console.log を出力しない系の設定 -----
+
+      return config;
+    },
+  };
+};
 export default nextConfig;
